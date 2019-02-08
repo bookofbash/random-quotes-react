95a211096278c4d3f062782ed5799575f977a919
Bashir <harrell.bashir@yahoo.com>
2019-02-09 00:55:01 +0800

    Updates

---
+++ precache-manifest.7973c86288656c2260677f39db13ce25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8135b646e6d05a33284",
+    "url": "/bookofbash/random-quotes-react/static/js/runtime~main.e8135b64.js"
+  },
+  {
+    "revision": "1b977399a44b86c5c35d",
+    "url": "/bookofbash/random-quotes-react/static/js/main.1b977399.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/bookofbash/random-quotes-react/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "1b977399a44b86c5c35d",
+    "url": "/bookofbash/random-quotes-react/static/css/main.6a57a485.chunk.css"
+  },
+  {
+    "revision": "01b1a683fa4ba1bb526746b011fe9f11",
+    "url": "/bookofbash/random-quotes-react/index.html"
+  }
+];
